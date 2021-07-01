f1f8f24c17edceceecfcca848ddb00457f1fac51
Jennifer Nolasco <nolasco.jennifer00@gmail.com>
2021-07-01 10:10:15 -0400

    Initial Commit

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by Grace on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
